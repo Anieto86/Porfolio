a176159f253ae5a970dae4bcbe2b602988b4931c
Alvaro Nieto <60838509+Anieto86@users.noreply.github.com>
2020-06-11 17:21:24 +0930

    porfolio handlebars

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Dependecies
 const express = require('express');
 const exphbs = require('express-handlebars');
-
+const path = require('path');
 
 // Create an instance of the express app.
 const app = express();
@@ -12,22 +12,29 @@ var PORT = process.env.PORT || 8080;
 
 
 //For use handlebar templated engine //  expbs()) this invoque handlebars
-app.engine("handlebars", exphbs({defalutLayout:"main"}));
+app.engine("handlebars", exphbs({defalutLayout:"main", 
+layoutsDir: path.join(__dirname, 'views/mainLayouts') // here I can change the name of layouts file for another name, in this case is the same same 
+})); // con esto lo que hacemos es decir a main.handeblar que sea el templado por default // ademas es importante tener la carpeta layouts y adentro main handelbars. 
 app.set("view engine" , "handlebars"); // set view engine to se or look for any file studies
 
 
 // Routing for render 
-app.get('/', (req, res) => {
-    res.render("home"); // here we point the main.handlebars is not necesary put ".handlebars"
+
+app.get("/" , (req,res) =>{
+res.render("home", {
+    title:"Home Page" , 
+    name:"Alvaro Nieto"});
 });
 
-// app.get('/', (req, res) => {
-//     res.render("porfolio"); 
-// });
+app.get("/contact", (req, res) => {
+    res.render("contact", {title:"Contact"})
+}); 
 
-// app.get('/', (req, res) => {
-//     res.render("contact"); 
-// });
+
+
+app.get("/porfolio", (req, res) => {
+    res.render("porfolio"); 
+});
 
 
 
